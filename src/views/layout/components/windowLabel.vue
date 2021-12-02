df12a4e95c9d0ae562217febed3cb01f4e52f29f
dingqi <632235874@qq.com>
2021-12-02 17:30:45 +0800

    dingqi-2021.11.29--会员签到： 签到配置 连续签到奖励 会员签到记录

---
+++ src/views/layout/components/windowLabel.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="window-label"></div>
+</template>
+
+<script>
+export default {
+  name: "windowLabel",
+  data () {
+    return {
+    }
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+#window-label{
+
+}
+</style>

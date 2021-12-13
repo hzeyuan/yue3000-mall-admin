eb0499a74d32d5a297e543c4692bea4d25eed7ff
dingqi <632235874@qq.com>
2021-12-13 09:49:55 +0800

    fix

--- src/views/user/level/index.vue
+++ src/views/user/level/index.vue
@@ -3,114 +3,156 @@
     <strapi-table
       model="users"
       :router="router"
+      :querys="querys"
       :columns="columns"
       :rowBars="rowBars"
       :diyBars="diyBars"
     />
   </div>
 </template>
-
 <script>
-  import { userLevelList as fetchList, deleteUserLevel } from '@/api/user'
-  import { formatDate } from '@/utils/date'
-  import strapiTable from '@/components/strapi-table'
+import {userLevelList as fetchList, deleteUserLevel} from '@/api/user'
+import {formatDate} from '@/utils/date'
+import strapiTable from '@/components/strapi-table'
+import _ from "lodash";
 
-  const defaultListQuery = {
-    page: 1,
-    pageSize: 10,
-    orderSn: null,
-    receiverKeyword: null,
-    status: null,
-    orderType: null,
-    sourceType: null,
-    createTime: null,
-  }
-  export default {
-    name: 'userLevel',
-    components: { strapiTable },
-    data() {
-      return {
-        router: {
-          find: {
-            url: '/mall-admin/user_levels',
-            method: 'get',
-          },
-          add: {
-            url: '/mall-admin/user_levels',
-            method: 'post',
-          },
-          update: {
-            url: '/mall-admin/user_level/:id',
-            method: 'put',
-          },
-          delete: {
-            url: '/mall-admin/user_level/:id',
-            method: 'delete',
-          },
+const defaultListQuery = {
+  page: 1,
+  pageSize: 10,
+  orderSn: null,
+  receiverKeyword: null,
+  status: null,
+  orderType: null,
+  sourceType: null,
+  createTime: null,
+}
+export default {
+  name: 'userLevel',
+  components: {strapiTable},
+  data() {
+    return {
+      router: {
+        find: {
+          url: '/mall-admin/user_levels',
+          method: 'get',
         },
-        // 字段配置信息
-        columns: [
-          {
-            key: 'id',
-            label: '序号',
-            type: Number,
-            componentType: 'text',
-            hidden: false,
+        add: {
+          url: '/mall-admin/user_levels',
+          method: 'post',
+        },
+        update: {
+          url: '/mall-admin/user_level/:id',
+          method: 'put',
+        },
+        delete: {
+          url: '/mall-admin/user_level/:id',
+          method: 'delete',
+        },
+      },
+      // 字段配置信息
+      columns: [
+        {
+          key: 'id',
+          label: '序号',
+          type: Number,
+          componentType: 'text',
+          hidden: false,
+        },
+        {
+          key: 'name',
+          label: '等级名称',
+          type: String,
+          componentType: 'input',
+          hidden: false,
+        },
+        {
+          key: 'image',
+          label: '等级图标',
+          componentType: 'upload-image',
+          type: String,
+          customAttrs: {},
+          hidden: false,
+        },
+        {
+          key: 'background_image',
+          label: '等级背景图',
+          type: Number,
+          componentType: 'image',
+          hidden: false,
+        },
+        {
+          key: 'growth_value',
+          label: '成长值',
+          type: Number,
+          componentType: 'text',
+          hidden: false,
+        },
+        {
+          key: 'privilege',
+          label: '权益列表',
+          type: Array,
+          componentType: 'select',
+          customAttrs: {
+            multiple: true
           },
-          {
-            key: 'name',
-            label: '等级名称',
-            type: String,
-            componentType: 'input',
-            hidden: false,
-          },
-          {
-            key: 'image',
-            label: '等级图标',
-            componentType: 'upload-image',
-            type: String,
-            customAttrs: {},
-            hidden: false,
-          },
-          {
-            key: 'background_image',
-            label: '等级背景图',
-            type: Number,
-            componentType: 'image',
-            hidden: false,
-          },
-          {
-            key: 'growth_value',
-            label: '成长值',
-            type: Number,
-            componentType: 'text',
-            hidden: false,
-          },
-          {
-            key: 'userCount',
-            label: '用户数',
-            type: Number,
-            componentType: 'text',
-            hidden: false,
-          },
-        ],
-        // 每行操作栏
-        rowBars: ['detail', 'delete'],
+          options: [
+            {
+              label: '购物折扣',
+              value: 1
+            },
+            {
+              label: '全场免邮',
+              value: 2
+            },
+            {
+              label: '会员优惠卷',
+              value: 3
+            },
+            {
+              label: '专属客服',
+              value: 4
+            },
+          ],
+          hidden: false,
+        },
+        {
+          key: 'userCount',
+          label: '用户数',
+          type: Number,
+          componentType: 'text',
+          hidden: false,
+        },
+      ],
+      // 每行操作栏
+      rowBars: ['detail', 'delete'],
 
-        querys: {},
+      querys: {
+        privilege: '',
+        privilege_list: []
+      },
+      privilegeList: [
+        {name: '购物折扣', id: 1},
+        {name: '全场免邮', id: 2},
+        {name: '会员优惠卷', id: 3},
+        {name: '专属客服', id: 4},
+      ],
 
-        diyBars: [],
-      }
-    },
-    methods: {},
-  }
+      diyBars: [],
+    }
+  },
+  methods: {
+    hand() {
+      console.log('点击');
+    }
+  },
+}
 </script>
 <style scoped>
-  .input-width {
-    width: 203px;
-  }
+.input-width {
+  width: 203px;
+}
 
-  body {
-    margin: 0;
-  }
+body {
+  margin: 0;
+}
 </style>
--- src/views/user/privilege/index.vue
+++ src/views/user/privilege/index.vue
@@ -5,47 +5,47 @@
 </template>
 
 <script>
-  import { formatDate } from '@/utils/date'
-  import strapiTable from '@/components/strapi-table'
+import {formatDate} from '@/utils/date'
+import strapiTable from '@/components/strapi-table'
 
-  export default {
-    components: { strapiTable },
-    data() {
-      return {
-        model: 'user-privilege',
-        columns: [
-          {
-            key: 'id',
-            label: 'id',
-            type: Number,
-            hidden: true,
-          },
-          {
-            key: 'name',
-            label: '名称',
-            type: String,
-            componentType: 'input',
-          },
-          {
-            key: 'image',
-            label: '权益图标',
-            componentType: 'upload-image',
-            type: String,
-            customAttrs: {},
-          },
-          {
-            key: 'remark',
-            label: '权益说明',
-            width: '180px',
-            componentType: 'textarea',
-            type: String,
-            defaultValue: '暂无',
-          },
-        ],
-      }
-    },
-    methods: {},
-  }
+export default {
+  components: {strapiTable},
+  data() {
+    return {
+      model: 'user-privilege',
+      columns: [
+        {
+          key: 'id',
+          label: '序号',
+          type: Number,
+          hidden: false,
+        },
+        {
+          key: 'name',
+          label: '名称',
+          type: String,
+          componentType: 'input',
+        },
+        {
+          key: 'image',
+          label: '权益图标',
+          componentType: 'upload-image',
+          type: String,
+          customAttrs: {},
+        },
+        {
+          key: 'remark',
+          label: '权益说明',
+          width: '180px',
+          componentType: 'textarea',
+          type: String,
+          defaultValue: '暂无',
+        },
+      ],
+    }
+  },
+  methods: {},
+}
 </script>
 
 <style></style>

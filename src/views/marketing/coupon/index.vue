ec695717d3c9f707641e8bf34b99825c4786d1ed
dingqi <632235874@qq.com>
2021-12-14 10:12:51 +0800

    dingQi-2021.12.14 修复组件bug

--- src/views/marketing/coupon/index.vue
+++ src/views/marketing/coupon/index.vue
@@ -20,6 +20,7 @@
           </el-descriptions-item>
         </el-descriptions>
       </template>
+
       <template slot="rules" slot-scope="{ row }">
         <el-descriptions :column="1">
           <el-descriptions-item label="使用门槛">
@@ -40,109 +41,109 @@
 </template>
 
 <script>
-  import strapiTable from '@/components/strapi-table'
-  export default {
-    components: { strapiTable },
-    data() {
-      return {
-        model: 'coupon',
-        columns: [
-          {
-            key: 'id',
-            label: 'id',
-            type: Number,
-            hidden: true,
-          },
-          {
-            key: 'name',
-            label: '名称',
-            type: String,
-            componentType: 'input',
-          },
-          {
-            key: 'discount',
-            label: '面额',
-            componentType: 'text',
-            displayFormatter: (value) => `￥${value}`,
-            type: String,
-            customAttrs: {},
-          },
-          {
-            key: 'use_time',
-            label: '有效期',
-            componentType: 'datetime-range',
-            width: '240px',
-            type: String,
-            isNoWrapper: true,
-            defaultValue: '暂无',
+import strapiTable from '@/components/strapi-table'
 
-            customAttrs: {
-              disabled: true,
-              'start-placeholder': '开始日期',
-              'end-placeholder': '结束日期',
-              size: 'mini',
-              'range-separator': '至',
+export default {
+  components: {strapiTable},
+  data() {
+    return {
+      model: 'coupon',
+      columns: [
+        {
+          key: 'id',
+          label: 'id',
+          type: Number,
+          hidden: true,
+        },
+        {
+          key: 'name',
+          label: '名称',
+          type: String,
+          componentType: 'input',
+        },
+        {
+          key: 'discount',
+          label: '面额',
+          componentType: 'text',
+          displayFormatter: (value) => `￥${value}`,
+          type: String,
+          customAttrs: {},
+        },
+        {
+          key: 'use_time',
+          label: '有效期',
+          componentType: 'datetime-range',
+          width: '240px',
+          type: String,
+          isNoWrapper: true,
+          defaultValue: '暂无',
+
+          customAttrs: {
+            disabled: true,
+            'start-placeholder': '开始日期',
+            'end-placeholder': '结束日期',
+            size: 'mini',
+            'range-separator': '至',
+          },
+        },
+        {
+          key: 'desc',
+          label: '优惠券描述',
+          componentType: 'textarea',
+          type: String,
+          customAttrs: {},
+        },
+        {
+          key: 'code',
+          label: '优惠码',
+          hidden: true,
+          search: true,
+          qs: 'code_eq',
+          searchCompoent: {
+            name: 'el-input',
+            attr: {
+              placeholder: '请输入折扣码',
             },
           },
-          {
-            key: 'desc',
-            label: '优惠券描述',
-            componentType: 'textarea',
-            type: String,
+          type: String,
+        },
+        {
+          key: 'rules',
+          label: '使用规则',
+          width: '180px',
+          componentType: 'text',
+          type: String,
+          customAttrs: {},
+        },
+        {
+          key: 'total',
+          label: '发行状况',
+          componentType: 'text',
+          type: String,
+          defaultValue: '暂无',
+        },
 
-            customAttrs: {},
-          },
-          {
-            key: 'code',
-            label: '优惠码',
-            hidden: true,
-            search: true,
-            qs: 'code_eq',
-            searchCompoent: {
-              name: 'el-input',
-              attr: {
-                placeholder: '请输入折扣码',
-              },
-            },
-            type: String,
-          },
-          {
-            key: 'rules',
-            label: '使用规则',
-            width: '180px',
-            componentType: 'text',
-            type: String,
-            customAttrs: {},
-          },
-          {
-            key: 'total',
-            label: '发行状况',
-            componentType: 'text',
-            type: String,
-            defaultValue: '暂无',
-          },
-
-          {
-            key: 'created_at',
-            label: '创建日期',
-            componentType: 'datetime-text',
-            type: String,
-            defaultValue: '暂无',
-          },
-        ],
-        rowBars: { delete: { name: '结束活动' } },
+        {
+          key: 'created_at',
+          label: '创建日期',
+          componentType: 'datetime-text',
+          type: String,
+          defaultValue: '暂无',
+        },
+      ],
+      rowBars: {delete: {name: '结束活动'}},
+    }
+  },
+  methods: {
+    threshold(row) {
+      if (row.discount < 1) {
+        return `满 ${row.min} 打 ${row.discount}折`
+      } else {
+        return `满 ${row.min}元 减 ${row.discount}元`
       }
     },
-    methods: {
-      threshold(row) {
-        if (row.discount < 1) {
-          return `满 ${row.min} 打 ${row.discount}折`
-        } else {
-          return `满 ${row.min}元 减 ${row.discount}元`
-        }
-      },
-    },
-  }
+  },
+}
 </script>
 
 <style></style>

2e0917c18df0651453241ccf11a648e650d78fb8
dingqi <632235874@qq.com>
2021-12-15 17:32:27 +0800

    dingQi-2021.12.14 抽屉内嵌表格处理

--- src/views/marketing/distribution/earningsDetailDrawer.vue
+++ src/views/marketing/distribution/earningsDetailDrawer.vue
@@ -1,28 +1,89 @@
 <template>
   <el-drawer
-    title="收入明细"
     :visible.sync="visible"
     direction="rtl"
-  ></el-drawer>
+  >
+    <div style="padding: 0 15px; width: 100%">
+      <div class="el-descriptions__title">收入明细：</div>
+      <el-table :data="earningsDetailData" v-loading="loading" align="center" width="100">
+        <el-table-column
+          prop="distribution_id"
+          width="50"
+          label="编号">
+        </el-table-column>
+        <el-table-column
+          prop="type"
+          width="80"
+          label="收入类型">
+        </el-table-column>
+        <el-table-column
+          prop="money"
+          width="80"
+          label="收入金额">
+        </el-table-column>
+        <el-table-column
+          prop="order_sn"
+          width="150"
+          label="订单编号">
+        </el-table-column>
+        <el-table-column
+          prop="created_at"
+          label="记录时间">
+          <template slot-scope="scope">
+            <span>{{ scope.row.created_at | date }}</span>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </el-drawer>
 </template>
 
 <script>
-  import { earningsDetail } from '@/api/user'
-  export default {
-    name: 'earningsDetail',
-    data() {
-      return {
-        visible: false,
-      }
-    },
-    watch: {},
-    methods: {
-      async open(id) {
-        this.visible = true
-        await earningsDetail(id)
-      },
-    },
-  }
+import {earningsDetail} from '@/api/user'
+
+export default {
+  name: 'earningsDetail',
+  data() {
+    return {
+      tableLoading: false,
+      visible: true,
+      earningsDetailData: [
+        {
+          created_at: "2021-11-26T14:13:11.000Z",
+          distribution_id: 12,
+          money: 199,
+          order_sn: "20211201621565",
+          sn: "",
+          type: "分销佣金",
+        },
+        {
+          created_at: "2021-11-26T14:13:11.000Z",
+          distribution_id: 12,
+          money: 199,
+          order_sn: "20211201621565",
+          sn: "",
+          type: "分销佣金",
+        },
+      ],
+    }
+  },
+  filters: {
+    date(value) {
+      let date = new Date(value)
+      return date.toLocaleString().replace(/:\d{1,2}$/, ' ')
+    }
+  },
+  watch: {},
+  methods: {
+    async open(id) {
+      this.tableLoading = true
+      this.visible = true
+      const res = await earningsDetail(id)
+      this.earningsDetailData = res.list
+      this.tableLoading = false
+    }
+  },
+}
 </script>
 
 <style></style>
--- src/views/marketing/distribution/fansDrawer.vue
+++ src/views/marketing/distribution/fansDrawer.vue
@@ -1,26 +1,81 @@
 <template>
-  <el-drawer title="粉丝列表" :visible.sync="visible" direction="rtl">
-    123
+  <el-drawer
+    :visible.sync="visible"
+    direction="rtl">
+    <div style="padding: 0 40px; width: 100%">
+      <div class="el-descriptions__title">粉丝列表：</div>
+      <el-table :data="fansDetailData" v-loading="loading" width="100">
+        <el-table-column
+          label="粉丝信息">
+          <template slot-scope="scope">
+            <div class="flex text-xs items-start">
+              <el-image :src="scope.row.avatar" lazy></el-image>
+              <div class="pl-2 text-left">
+                <p>会员id:{{ scope.row.user_id }}</p>
+                <p>会员昵称:{{ scope.row.username }}</p>
+                <p>会员等级:{{ scope.row.level }}</p>
+              </div>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="营销信息">
+          <template slot-scope="scope">
+            <div class="text-xs items-start">
+              <div class="pl-2 text-left">
+                <p>分销数量:{{ scope.row.distribution_order_num }}</p>
+                <p>分销金额:{{ scope.row.distribution_money }}</p>
+                <p>订单金额:{{ scope.row.order_amount }}</p>
+              </div>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </el-drawer>
 </template>
 
 <script>
-  import { fans } from '@/api/user'
-  export default {
-    name: 'earningsDetail',
-    data() {
-      return {
-        visible: false,
-      }
+import {fans} from '@/api/user'
+
+export default {
+  name: 'fansDetail',
+  data() {
+    return {
+      tableLoading: false,
+      visible: false,
+      fansDetailData: [
+        {
+          avatar: "blob:http://localhost:1024/d094cbe7-c7f3-4c56-8f5f-c1829131e066",
+          created_at: null,
+          distribution_money: 0,
+          distribution_order_num: 0,
+          leader: {
+            id: 21,
+            level: "黄金会员",
+            phone: "11111111111",
+            sn: "654321",
+            username: "小黄同学",
+          },
+          order_amount: 0,
+          phone: "33333333333",
+          sn: "416521",
+          username: "hdh ",
+        }
+      ]
+    }
+  },
+  watch: {},
+  methods: {
+    async open(id) {
+      this.tableLoading = true
+      this.visible = true
+      const res = await fans(id)
+      this.fansDetailData = res.list
+      this.tableLoading = false
     },
-    watch: {},
-    methods: {
-      async open(id) {
-        this.visible = true
-        await fans(id)
-      },
-    },
-  }
+  },
+}
 </script>
 
 <style></style>
